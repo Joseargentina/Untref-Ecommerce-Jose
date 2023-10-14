cf30ab85523f0c51ff9b487880dee65f15e84c34
Joseargentina <josebarone87@hotmail.com>
2023-10-14 02:43:59 -0300

    agregue contenido de js al carrito

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-import { traerDatos, actualizarContenidoCarrito } from './funciones.js';
+import { traerDatos, actualizarContenidoCarrito, actualizarContenidoPrecioFinal, agregarEventoEliminarProducto, finalizarCompra, sumarProductos, mostrarPrecioFinal, mostrarPrecioProducto } from './funciones.js';
 
 traerDatos()
     .then(function (datos) {
@@ -15,6 +15,7 @@ traerDatos()
     const carrito = JSON.parse(localStorage.getItem('carrito'));
     const contenedorCarrito = document.getElementById('contenedor-carrito');
     let contenidoHTML = ''; //variable para almacenar el contenido HTML del contenedor-carrito
+
     
 
     if (!Array.isArray(carrito) || carrito.length === 0) {
@@ -23,74 +24,19 @@ traerDatos()
             title: 'Oops...',
             text: 'El carrito se encuentra vacio!'
         })
+        contenidoHTML = actualizarContenidoCarrito();
     } else {
         carrito.forEach(function (producto) {
-            // Llamar a la funcion actualizarContenedorCarrito y asignar el contenido HTML devuelto
-            contenidoHTML = actualizarContenidoCarrito();
-
-            // Agregar el contenido HTML al contenedor
+            // Llamar a la funcion actualizarContenedorCarrito y actualizarContenidoPrecioFinal y asignar el contenido HTML devuelto
+            contenidoHTML = actualizarContenidoCarrito() + actualizarContenidoPrecioFinal();
             contenedorCarrito.innerHTML = contenidoHTML;
-
-            const contenedorPrecio = document.createElement('article');
-            contenedorPrecio.className = 'contenedor-producto contenedor-total-productos';
-            contenedorCarrito.appendChild(contenedorPrecio);
-            contenedorPrecio.innerHTML = `
-                    <h3>Total Productos</h3>
-                    <span class="precio-final">$ 151515</span>`;
-            const contenedorFinalizar = document.createElement('form');
-            contenedorFinalizar.className = 'contenedor-producto contenedor-finalizar-compra';
-            contenedorCarrito.appendChild(contenedorFinalizar);
-            contenedorFinalizar.innerHTML = `
-                    <button type=submit class="btn btn-finalizar-compra">Finalizar compra</button>`;
-            const btnFinalizarCompra = document.querySelector(".btn-finalizar-compra");
             
-            //Agregar evento de click al boton de finalizar compra
-            btnFinalizarCompra.addEventListener('click', function(e){
-                e.preventDefault();
-                console.log(e.target)
-            });
-        });
-
-        function agregarEventoEliminarProducto() {
-            const carritoGuardado = JSON.parse(localStorage.getItem('carrito'));
-            const contenedorCarrito = document.getElementById('contenedor-carrito');
-            
-            //Agrego evento de click al contenedorCarrito
-            contenedorCarrito.addEventListener('click', function (e) {
-                //Si el click se hace sobre el btn-eliminar
-                if (e.target.classList.contains('btn-eliminar')) {
-                    e.preventDefault();
-                    const carrito = JSON.parse(localStorage.getItem('carrito'));
-                    const botonesEliminar = document.querySelectorAll('.btn-eliminar');
-                    // Obtener el índice del elemento actual dentro de la lista de botonesEliminar
-                    const indiceEliminar = Array.from(botonesEliminar).indexOf(e.target);
-                    let contadorProductos = document.getElementById('contadorProductos');
-                    
-                    if (indiceEliminar !== -1) {
-                        if (carrito[indiceEliminar].cantidad > 1) {
-                            carrito[indiceEliminar].cantidad--;
-                            localStorage.setItem('carrito', JSON.stringify(carrito));
-                            contenidoHTML = actualizarContenidoCarrito();
-                        } else {
-                            carrito.splice(indiceEliminar, 1);
-                            localStorage.setItem('carrito', JSON.stringify(carrito));
-                            contenidoHTML = actualizarContenidoCarrito();
-                        }
-                    }
-                    
-                    contenidoHTML = actualizarContenidoCarrito();
-                    contenedorCarrito.innerHTML = contenidoHTML;
-                    //Suma de la cantidad total de productos
-                    const sumaCantidades = carrito.reduce((suma, producto) => suma + producto.cantidad, 0);
-                    contadorProductos.textContent = sumaCantidades;
-                    
-                    localStorage.setItem('carrito', JSON.stringify(carrito));
-                    localStorage.setItem('contadorProductos', sumaCantidades);
-                    console.log(sumaCantidades)
-                    console.log(carrito);
-                }
-            });
-        }
-        // Llamar a la función agregarEventoEliminarProducto fuera del bucle forEach
+        })
+        // Llamar a la función agregarEventoEliminarProducto
         agregarEventoEliminarProducto();
-    }
+        mostrarPrecioFinal();
+        mostrarPrecioProducto();
+
+
+        finalizarCompra();
+}
--- js/funciones.js
+++ js/funciones.js
@@ -137,20 +137,172 @@ export function actualizarContenidoCarrito(){
             <figcaption>
                 <h3>${producto.titulo}</h3>
                 <p>cantidad: ${producto.cantidad}</p>
-                <li>Precio: $${producto.precio}</li>
+                <li class="precio-producto-final" data-nombre="producto1">Total ${producto.precio}</li>
                 <i class="fa-solid fa-trash-can btn-eliminar"></i>
             </figcaption>
         </article>
         `;
     });
+    
     return contenidoHTML; // Devuelve el contenido HTML generado
 }
 
-//Funcion para eliminar un producto
-export function eliminarProducto(indice) {
+//Funcion para actualizar los productos del carrito
+export function actualizarContenidoPrecioFinal(){
+    const contenedorCarrito = document.getElementById('contenedor-carrito');
+    let contenidoHTMLprecioFinal = '';
     const carrito = JSON.parse(localStorage.getItem('carrito'));
-    console.log(indice);    
-    carrito.splice(indice, 1);
-    actualizarContenidoCarrito();
+    console.log(carrito);
+        contenidoHTMLprecioFinal += `
+        <article class="contenedor-producto contenedor-total-productos">
+            <h3>Total Productos</h3>
+            <span class="precio-final">$ 15151</span>
+        </article>
+        <form class=" contenedor-finalizar-compra">
+                <button type=submit class="btn btn-finalizar-compra">Finalizar compra</button>
+        </form>
+        `;
+    return contenidoHTMLprecioFinal; // Devuelve el contenido HTML generado
 }
 
+//Funcion para agregar evento de eliminar a los productos
+let contenidoHTML = ''; //variable para almacenar el contenido HTML del contenedor-carrito
+export function agregarEventoEliminarProducto() {
+    const contenedorCarrito = document.getElementById('contenedor-carrito');
+
+    //Agrego evento de click al contenedorCarrito
+    contenedorCarrito.addEventListener('click', function (e) {
+        //Si el click se hace sobre el btn-eliminar
+        if (e.target.classList.contains('btn-eliminar')) {
+            e.preventDefault();
+            const carrito = JSON.parse(localStorage.getItem('carrito'));
+            const botonesEliminar = document.querySelectorAll('.btn-eliminar');
+            // Obtener el índice del elemento actual dentro de la lista de botonesEliminar
+            const indiceEliminar = Array.from(botonesEliminar).indexOf(e.target);//Devuel -1 si encuentra el indice
+            
+            if (indiceEliminar !== -1) {
+                if (carrito[indiceEliminar].cantidad > 1) {
+                    carrito[indiceEliminar].cantidad--;
+                    localStorage.setItem('carrito', JSON.stringify(carrito));
+                    mostrarPrecioProducto();
+                    mostrarPrecioFinal();
+                } else {
+                    carrito.splice(indiceEliminar, 1);
+                    localStorage.setItem('carrito', JSON.stringify(carrito));
+                }
+            }
+
+            
+            contenidoHTML = actualizarContenidoCarrito() ;
+            if (carrito.length > 0) {
+                contenidoHTML += actualizarContenidoPrecioFinal();
+                contenedorCarrito.innerHTML = contenidoHTML;
+            }else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'El carrito se encuentra vacio!'
+                })
+            }
+            contenedorCarrito.innerHTML = contenidoHTML;
+            //Suma de la cantidad total de productos
+            const sumaCantidades = carrito.reduce((suma, producto) => suma + producto.cantidad, 0);
+            let contadorProductos = document.getElementById('contadorProductos');
+            contadorProductos.textContent = sumaCantidades;
+            
+            localStorage.setItem('carrito', JSON.stringify(carrito));
+            localStorage.setItem('contadorProductos', sumaCantidades);
+            console.log(sumaCantidades)
+            console.log(carrito);
+        }
+        mostrarPrecioFinal();
+        mostrarPrecioProducto();
+    });
+}
+
+export function finalizarCompra() {
+    const btnFinalizarCompra = document.querySelector(".btn-finalizar-compra");
+    // Agregar evento de click al botón de finalizar compra
+    btnFinalizarCompra.addEventListener('click', function(e) {
+        e.preventDefault();
+        // Mostrar una ventana emergente para solicitar el nombre de usuario y el número de tarjeta de crédito
+        Swal.fire({
+            title: 'Ingrese su nombre de usuario y número de tarjeta de crédito',
+            html:
+            '<form>' +
+            '<input id="username" class="swal2-input" autocomplete="current-password" placeholder="Nombre de usuario">' +
+            '<input id="target-credit" type="password" autocomplete="current-password" class="swal2-input" placeholder="Número de tarjeta de crédito">' +
+            '</form>',
+            showCancelButton: true,
+            confirmButtonText: 'Confirmar',
+            cancelButtonText: 'Cancelar',
+            preConfirm: () => {
+                const username = Swal.getPopup().querySelector('#username').value;
+                const targetCredit = Swal.getPopup().querySelector('#target-credit').value;
+                if (!username || !targetCredit) {
+                    Swal.showValidationMessage('Debe ingresar tanto el nombre de usuario como el número de tarjeta de crédito');
+                }
+                return { username: username, targetCredit: targetCredit };
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const username = result.value.username;
+                const targetCredit = result.value.targetCredit;
+                // Guardar los datos en el localStorage
+                localStorage.setItem("username", username);
+                localStorage.setItem("targetCredit", targetCredit);
+                // Realizar acciones adicionales al finalizar la compra
+                // Aquí puedes agregar tu lógica personalizada utilizando el nombre de usuario y el número de tarjeta de crédito
+                console.log(`Compra finalizada por ${username} con número de tarjeta de crédito ${targetCredit}`);
+                Swal.fire(
+                    '¡Buen trabajo!',
+                    '¡Has finalizado la compra!',
+                    'success'
+                );
+            }
+        });
+    });
+}
+
+// Funcion para sumar el total de los productos
+
+export function sumarProductos (){
+    const carrito =JSON.parse(localStorage.getItem('carrito'));
+    // Obtener los precios de los productos en el carrito
+    const precios = carrito.map(producto => producto.precio * producto.cantidad);
+
+    // Calcular la suma total de los precios
+    const sumaTotal = precios.reduce((suma, precio) => suma + precio, 0);
+
+    // Mostrar la suma total en el contenedor adecuado
+    const contenedorSumaTotal = document.querySelector('.precio-final');
+    contenedorSumaTotal.innerHTML = `${sumaTotal}`;
+
+    return sumaTotal;
+}
+
+//Funcion para mostrar el precio final
+// Mostrar la suma total en el elemento span de precio final
+export function mostrarPrecioFinal() {
+    // Obtengo el elemento para mostrar el precio final
+    const precioFinalProducto = document.querySelector('.precio-final');
+    const sumaTotal = sumarProductos();
+    precioFinalProducto.textContent = `$ ${sumaTotal}`;
+    }
+
+    export function mostrarPrecioProducto() {
+        const carrito = JSON.parse(localStorage.getItem('carrito'));
+        const precioFinalProductos = document.querySelectorAll('.precio-producto-final');
+        carrito.forEach((elemento, index) => {
+            const nombreProducto = elemento.titulo;
+            console.log(nombreProducto);
+            const cantidadProducto = elemento.cantidad;
+            console.log(cantidadProducto);
+            const precioProducto = elemento.precio;
+            console.log(precioProducto);
+            const subtotalProducto = precioProducto * cantidadProducto; // Si cantidadProducto es undefined, se asigna 0
+        
+                precioFinalProductos[index].textContent = `Total $${subtotalProducto}`;
+            
+        });
+    }
